4cfe12ea802f6edad1fcb29e15560bf612615f0c
will <w.duffy@outlook.com>
2024-06-30 03:22:37 -0500

    adds pusher

--- server/src/push.ts
+++ server/src/push.ts
@@ -2,6 +2,7 @@ import {serverID, tx, type Transaction} from './db';
 import type {MessageWithID} from 'shared';
 import type {MutationV1, PushRequestV1} from 'replicache';
 import type {Request, Response, NextFunction} from 'express';
+import Pusher from 'pusher';
 
 export async function handlePush(
   req: Request,
@@ -182,5 +183,22 @@ async function createMessage(
 }
 
 async function sendPoke() {
-  // TODO
-}
+    if (
+      !process.env.REPLICHAT_PUSHER_APP_ID ||
+      !process.env.REPLICHAT_PUSHER_KEY ||
+      !process.env.REPLICHAT_PUSHER_SECRET ||
+      !process.env.REPLICHAT_PUSHER_CLUSTER
+    ) {
+      throw new Error('Missing Pusher environment variables');
+    }
+    const pusher = new Pusher({
+      appId: process.env.REPLICHAT_PUSHER_APP_ID,
+      key: process.env.REPLICHAT_PUSHER_KEY,
+      secret: process.env.REPLICHAT_PUSHER_SECRET,
+      cluster: process.env.REPLICHAT_PUSHER_CLUSTER,
+      useTLS: true,
+    });
+    const t0 = Date.now();
+    await pusher.trigger('default', 'poke', {});
+    console.log('Sent poke in', Date.now() - t0);
+  }
